9474b4a2485634b9538c6bb3cf027e31d2889c95
Deivis Herrera <deivisaherreraj@gmail.com>
2026-09-04 18:11:20 -0500

    chore: remove orphaned link components and constants

--- website_frontend/website_frontend/constants/site_constants.py
+++ website_frontend/website_frontend/constants/site_constants.py
@@ -5,8 +5,6 @@ AVAILABILITY_STATUS_DEFAULT = "activo"
 
 # Cabecera
 GITHUB_URL = "https://github.com/deivisaherreraj"
-TWITTER_X_URL = "/"
-INSTAGRAM_URL = "/"
 LINKEDIN_URL = "https://linkedin.com/in/deivisaherreraj"
 
 # Comunidad
@@ -14,22 +12,5 @@ TWITCH_URL = "https://twitch.tv/deivisaherreraj"
 YOUTUBE_URL = "https://youtube.com/@dherrerajdev"
 DISCORD_URL = "https://discord.gg/4pqbjXsrUX"
 
-# Plataformas de trabajo
-WORKANA_URL = "https://www.workana.com/freelancer/e466a72ac24f41aa356be17f406b3f9b"
-GUMROAD_URL = "https://dherrerajdev.gumroad.com/"
-
-# Recursos y más
-BOOK_URL = "/"
-BOOKS_URL = "/"
-SETUP_URL = "/setup"
-DEIVISAHERRERAJ_URL = "https://dherreraj.netlify.app"
-DHERRERAJDEV_URL = "#"
-COFFEE_URL = "https://www.buymeacoffee.com/dherrerajdev"
-REPO_URL = "https://github.com/deivisaherreraj/website"
-
 # Contacto
-MYPUBLICINBOX_URL = "/inbox"
 EMAIL = "deivisaherreraj@gmail.com"
-
-# Cursos
-CSHARP_COURSE_URL = "/"
